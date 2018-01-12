5ee125b7096760f620d1cb74cd85ef0fc5bf5db5
Yann Sionneau <yann@sionneau.net>
2018-01-12 15:44:52 +0100

    admin interface object listing clean up

--- pytition/petition/admin.py
+++ pytition/petition/admin.py
@@ -3,5 +3,11 @@ from django.contrib import admin
 from petition.models import Signature, Petition
 
 
-admin.site.register(Signature)
-admin.site.register(Petition)
+@admin.register(Signature)
+class SignatureAdmin(admin.ModelAdmin):
+    list_display =  ('first_name', 'last_name', 'phone', 'email', 'confirmed', 'subscribed_to_mailinglist', 'petition')
+
+
+@admin.register(Petition)
+class PetitionAdmin(admin.ModelAdmin):
+    pass
