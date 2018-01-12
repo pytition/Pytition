be87a388aa371bee7260715f025d731fe540d829
Yann Sionneau <yann@sionneau.net>
2018-01-12 15:43:15 +0100

    a bit of html encoding cleanup

--- pytition/petition/models.py
+++ pytition/petition/models.py
@@ -1,8 +1,11 @@
 from django.db import models
-from django.utils.html import mark_safe
+from django.utils.html import mark_safe, strip_tags
 
 from tinymce import models as tinymce_models
 from colorfield.fields import ColorField
+
+import html
+
 
 class Petition(models.Model):
 
@@ -37,10 +40,10 @@ class Petition(models.Model):
     twitter_image = models.CharField(max_length=500, blank=True)
 
     def __str__(self):
-        return mark_safe(self.title)
+        return html.unescape(mark_safe(strip_tags(self.title)))
 
     def __repr__(self):
-        return mark_safe(self.title)
+        return html.unescape(mark_safe(strip_tags(self.title)))
 
 
 class Signature(models.Model):
@@ -54,9 +57,9 @@ class Signature(models.Model):
     subscribed_to_mailinglist = models.BooleanField(default=False)
 
     def __str__(self):
-        return "[{}:{}] {} {}".format(self.petition.id, "OK" if self.confirmed else "..", self.first_name,
-                                      self.last_name)
+        return html.unescape("[{}:{}] {} {}".format(self.petition.id, "OK" if self.confirmed else "..", self.first_name,
+                                                    self.last_name))
 
     def __repr__(self):
-        return "[{}:{}] {} {}".format(self.petition.id, "OK" if self.confirmed else "..", self.first_name,
-                                      self.last_name)
+        return html.unescape("[{}:{}] {} {}".format(self.petition.id, "OK" if self.confirmed else "..", self.first_name,
+                                      self.last_name))
