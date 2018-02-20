fffd5bff3a4557d65a4a63bf7a2b271f6d717f7d
Yann Sionneau <yann@sionneau.net>
2018-02-20 14:38:36 +0100

    use mysql backend

--- pytition/pytition/settings.py
+++ pytition/pytition/settings.py
@@ -79,8 +79,11 @@ WSGI_APPLICATION = 'pytition.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/home/petition/www/my.cnf',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
