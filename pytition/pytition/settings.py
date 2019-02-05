fd8c63962be05fd336ac47341c5349c420305f9e
numahell <numahell@numajules.net>
2019-02-05 01:42:34 +0100

    Add allowed hosts

--- pytition/pytition/settings.py
+++ pytition/pytition/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'lbkzn($sn*oi!azcw022%=o+av8+iqjl&^q3gy)$j&r68qh^-4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
