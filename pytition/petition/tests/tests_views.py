34d0ec489c8e7e6888e8bd376b93ec5b90b95f1d
Emmanuelle Helly <emmanuelle.helly@makina-corpus.com>
2019-02-08 23:32:07 +0100

    Test index view, status code and petition number

--- pytition/petition/tests/tests_views.py
+++ pytition/petition/tests/tests_views.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from petition.models import Organization, Petition
 
-# Create your tests here.
+
+class ViewsTest(TestCase):
+    """Test views"""
+    @classmethod
+    def setUpTestData(cls):
+        petitions_nb = 9
+        for i in range(petitions_nb):
+            p = Petition.objects.create(title="Petition {i}", text=f"blabla {i}", published=True)
+
+    def tearDown(self):
+        # Clean up run after every test method.
+        pass
+
+    def test_index_200(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_petition_list(self):
+        response = self.client.get('/')
+        self.assertTrue(len(response.context['petitions']) == 9)
