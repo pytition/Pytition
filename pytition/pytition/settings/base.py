e995d0a14860511c5ce17d39e60cea9ac0f52de1
Yann Sionneau <yann@sionneau.net>
2019-02-13 18:28:58 +0100

    Use bcrypt to store passwords and IP address hashes

--- pytition/pytition/settings/base.py
+++ pytition/pytition/settings/base.py
@@ -56,6 +56,14 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+]
+
 ROOT_URLCONF = 'pytition.urls'
 
 TEMPLATES = [
