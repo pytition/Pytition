d1250b8f780c4b4f6aafe25cb5872d754e11a517
Tek <tek@randhome.io>
2019-04-26 13:59:10 +0200

    updates tests on views

--- pytition/petition/tests/tests_LeaveOrg.py
+++ pytition/petition/tests/tests_LeaveOrg.py
@@ -3,44 +3,19 @@ from django.urls import reverse
 from django.contrib.auth import get_user_model
 
 from petition.models import Organization, Petition, PytitionUser, Permission
+from .utils import add_default_data
 
-
-users = ['julia', 'john', 'max', 'sarah']
-orgs = ['RAP', 'Greenpeace', 'Attac', 'Les Amis de la Terre']
-
-org_members = {
-    'RAP': ['julia'],
-    'Les Amis de la Terre': ['julia', 'max'],
-    'Attac': ['john'],
-}
 
 class LeaveOrgViewTest(TestCase):
     """Test LeaveOrg view"""
     @classmethod
     def setUpTestData(cls):
-        User = get_user_model()
-        for org in orgs:
-            o = Organization.objects.create(name=org)
-            o.save()
-        for user in users:
-            u = User.objects.create_user(user, password=user)
-            u.first_name = user
-            u.last_name = user + "Last"
-            u.save()
-        for orgname in org_members:
-            org = Organization.objects.get(name=orgname)
-            for username in org_members[orgname]:
-                user = PytitionUser.objects.get(user__username=username)
-                user.organizations.add(org)
-                permission = Permission.objects.create(organization=org, can_modify_permissions=True)
-                permission.save()
-                user.permissions.add(permission)
-                user.save()
-
-        # give julia can_modify_petitions permission on "Les Amis de la Terre" organization
-        perm = PytitionUser.objects.get(user__username="julia").permissions\
-            .get(organization__name="Les Amis de la Terre")
-        perm.can_modify_petitions = True
+        add_default_data()
+        # Add max as admin of les amis de la terre
+        max = PytitionUser.objects.get(user__username="max")
+        org = Organization.objects.get(name="Les Amis de la Terre")
+        perm = Permission.objects.get(organization=org, user=max)
+        perm.can_modify_permissions = True
         perm.save()
 
     def login(self, name, password=None):
@@ -69,7 +44,7 @@ class LeaveOrgViewTest(TestCase):
         response = self.client.get(reverse("leave_org", args=[org.slugname]))
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, reverse("account_settings"))
-        self.assertNotIn(org, julia.organizations.all())
+        self.assertNotIn(org, julia.organization_set.all())
 
     def test_leave_refuse_alone(self):
         julia = self.login("julia")
@@ -77,5 +52,4 @@ class LeaveOrgViewTest(TestCase):
         response = self.client.get(reverse("leave_org", args=[org.slugname]))
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, reverse("account_settings") + '#a_org_form')
-        self.assertIn(org, julia.organizations.all())
-
+        self.assertIn(org, julia.organization_set.all())
--- pytition/petition/tests/tests_OrgCreateView.py
+++ pytition/petition/tests/tests_OrgCreateView.py
@@ -3,87 +3,15 @@ from django.urls import reverse
 from django.contrib.auth import get_user_model
 from django.utils.text import slugify
 
-from petition.models import Organization, Petition, PytitionUser
+from petition.models import Organization, Petition, PytitionUser, Permission
+from .utils import add_default_data
 
-
-users = ['julia', 'john', 'max', 'sarah']
-orgs = ['RAP', 'Greenpeace', 'Attac', 'Les Amis de la Terre']
-
-user_published_petitions = {
-    'john': 0,
-    'sarah': 0,
-    'julia': 5,
-    'max': 10
-}
-
-user_unpublished_petitions = {
-    'john': 0,
-    'sarah': 5,
-    'julia': 0,
-    'max': 10
-}
-
-org_published_petitions = {
-    'RAP': 0,
-    'Les Amis de la Terre': 0,
-    'Greenpeace': 1,
-    'Attac': 2
-}
-
-org_unpublished_petitions = {
-    'RAP': 0,
-    'Les Amis de la Terre': 1,
-    'Greenpeace': 0,
-    'Attac': 2
-}
-
-org_members = {
-    'RAP': ['julia'],
-    'Les Amis de la Terre': ['julia', 'max'],
-    'Attac': ['john'],
-}
 
 class OrgCreateViewTest(TestCase):
     """Test Org Creation view"""
     @classmethod
     def setUpTestData(cls):
-        User = get_user_model()
-        for org in orgs:
-            o = Organization.objects.create(name=org)
-            for i in range(org_published_petitions[org]):
-                p = Petition.objects.create(published=True)
-                o.petitions.add(p)
-                p.save()
-            for i in range(org_unpublished_petitions[org]):
-                p = Petition.objects.create(published=False)
-                o.petitions.add(p)
-                p.save()
-            o.save()
-        for user in users:
-            u = User.objects.create_user(user, password=user)
-            u.first_name = user
-            u.last_name = user + "Last"
-            u.save()
-            pu = PytitionUser.objects.get(user__username=user)
-            for i in range(user_published_petitions[user]):
-                p = Petition.objects.create(published=True)
-                pu.petitions.add(p)
-                p.save()
-            for i in range(user_unpublished_petitions[user]):
-                p = Petition.objects.create(published=False)
-                pu.petitions.add(p)
-                p.save()
-        for orgname in org_members:
-            org = Organization.objects.get(name=orgname)
-            for username in org_members[orgname]:
-                user = PytitionUser.objects.get(user__username=username)
-                org.add_member(user)
-
-        # give julia can_modify_petitions permission on "Les Amis de la Terre" organization
-        perm = PytitionUser.objects.get(user__username="julia").permissions\
-            .get(organization__name="Les Amis de la Terre")
-        perm.can_modify_petitions = True
-        perm.save()
+        add_default_data()
 
     def login(self, name, password=None):
         self.client.login(username=name, password=password if password else name)
@@ -100,14 +28,14 @@ class OrgCreateViewTest(TestCase):
     def test_CreateOK(self):
         john = self.login("john")
         newname = 'my new-org with @ ç special_chars'
-        previous_org_numbers = len(john.organizations.all())
+        previous_org_numbers = john.organization_set.count()
         data = {
             'name': newname,
         }
         response = self.client.post(reverse("org_create"), data, follow=True)
         self.assertRedirects(response, reverse("user_dashboard"))
         user = response.context['user']
-        orgs = user.organizations.all()
+        orgs = user.organization_set.all()
         self.assertEquals(user, john)
         self.assertEquals(len(orgs), previous_org_numbers + 1)
         org = Organization.objects.get(slugname=slugify(newname))
@@ -117,7 +45,7 @@ class OrgCreateViewTest(TestCase):
     def test_CreateAlreadyExistsKO(self):
         john = self.login("john")
         newname = 'my new-org with @ ç special_chars'
-        previous_org_numbers = len(john.organizations.all())
+        previous_org_numbers = john.organization_set.count()
         data = {
             'name': newname,
         }
@@ -125,7 +53,7 @@ class OrgCreateViewTest(TestCase):
         response = self.client.post(reverse("org_create"), data, follow=True)
         self.assertRedirects(response, reverse("user_dashboard"))
         user = response.context['user']
-        orgs = user.organizations.all()
+        orgs = user.organization_set.all()
         self.assertEquals(user, john)
         self.assertEquals(len(orgs), previous_org_numbers + 1)
         org = Organization.objects.get(slugname=slugify(newname))
@@ -135,20 +63,20 @@ class OrgCreateViewTest(TestCase):
         # second creation try
         response = self.client.post(reverse("org_create"), data, follow=True)
         self.assertEquals(response.status_code, 200)
-        org_count = user.organizations.filter(name=newname).count()
+        org_count = user.organization_set.filter(name=newname).count()
         self.assertEquals(org_count, 1)
 
     def test_CreateEmptyKO(self):
         john = self.login("john")
         newname = ''
-        previous_org_numbers = len(john.organizations.all())
+        previous_org_numbers = john.organization_set.count()
         data = {
             'name': newname,
         }
         response = self.client.post(reverse("org_create"), data)
         self.assertEquals(response.status_code, 200)
         user = response.context['user']
-        orgs = user.organizations.all()
+        orgs = user.organization_set.all()
         self.assertEquals(user, john)
         self.assertEquals(len(orgs), previous_org_numbers)
         empty_org_name_number = Organization.objects.filter(name='').count()
