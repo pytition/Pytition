aa5cea2fe9ae831dfce575a49709a048f81a7aa9
Yann Sionneau <yann@sionneau.net>
2019-05-09 08:17:47 +0200

    Fix migration

--- pytition/petition/migrations/0034_auto_20190419_1522.py
+++ pytition/petition/migrations/0034_auto_20190419_1522.py
@@ -22,7 +22,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='slugmodel',
             name='petition',
-            field=models.ForeignKey(default=0, on_delete=django.db.models.deletion.CASCADE, to='petition.Petition'),
+            field=models.ForeignKey(default=None, on_delete=django.db.models.deletion.CASCADE, to='petition.Petition'),
             preserve_default=False,
         ),
         migrations.DeleteModel(
