b336591955eead54e0afc7c895d92f88957a9a17
Martin Lehoux <martin@lehoux.net>
2020-05-12 08:44:12 -0700

    fix - Use new cli in prepopulate.sh

--- dev/prepopulate.sh
+++ dev/prepopulate.sh
@@ -2,31 +2,29 @@
 
 cd pytition
 echo "Creating 3 Organizations (RAP, Greenpeace and Attac)..."
-python3 ./cli_pytition.py gen_orga --orga RAP
-python3 ./cli_pytition.py gen_orga --orga Greenpeace
-python3 ./cli_pytition.py gen_orga --orga Attac
+python3 ./manage.py gen_orga RAP Greenpeace Attac
 
 echo "Creating 3 users (john, max, julia)..."
-python3 ./cli_pytition.py gen_user --username john --first-name John --last-name Smith -p john
-python3 ./cli_pytition.py gen_user --username max --first-name Max --last-name More -p max
-python3 ./cli_pytition.py gen_user --username julia --first-name Julia --last-name Steven -p julia
+python3 ./manage.py gen_user john john --first-name John --last-name Smith
+python3 ./manage.py gen_user max max --first-name Max --last-name More
+python3 ./manage.py gen_user julia julia --first-name Julia --last-name Steven
 
 echo "Make John join RAP and Greenpeace..."
-python3 ./cli_pytition.py join_org --orga RAP --user john
-python3 ./cli_pytition.py join_org --orga Greenpeace --user john
+python3 ./manage.py join_org john RAP 
+python3 ./manage.py join_org john Greenpeace 
 
 echo "Make Julia join Greenpeace and Attac"
-python3 ./cli_pytition.py join_org --orga Attac --user julia
-python3 ./cli_pytition.py join_org --orga Greenpeace --user julia
+python3 ./manage.py join_org julia Attac 
+python3 ./manage.py join_org julia Greenpeace
 
 echo "Make Max join Attac and RAP..."
-python3 ./cli_pytition.py join_org --orga Attac --user max
-python3 ./cli_pytition.py join_org --orga RAP --user max
+python3 ./manage.py join_org max Attac
+python3 ./manage.py join_org max RAP
 
 echo "Creating petitions for each user and each organization..."
-python3 ./cli_pytition.py generate_petitions -n 10 --orga RAP
-python3 ./cli_pytition.py generate_petitions -n 10 --orga Greenpeace
-python3 ./cli_pytition.py generate_petitions -n 10 --orga Attac
-python3 ./cli_pytition.py generate_petitions -n 10 --user john
-python3 ./cli_pytition.py generate_petitions -n 10 --user max
-python3 ./cli_pytition.py generate_petitions -n 10 --user julia
+python3 ./manage.py gen_pet -n 10 --orga RAP
+python3 ./manage.py gen_pet -n 10 --orga Greenpeace
+python3 ./manage.py gen_pet -n 10 --orga Attac
+python3 ./manage.py gen_pet -n 10 --user john
+python3 ./manage.py gen_pet -n 10 --user max
+python3 ./manage.py gen_pet -n 10 --user julia
