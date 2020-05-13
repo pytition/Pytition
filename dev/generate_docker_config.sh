b52ad9926b3cc796cac7737a14fd7c96cd43ffd4
simpnu <simon@pruneau.info>
2020-05-13 08:45:39 +0200

    Add Maildev as Fake SMTP for dev

--- dev/generate_docker_config.sh
+++ dev/generate_docker_config.sh
@@ -7,3 +7,5 @@ echo "SECRET_KEY = \"${SECRET_KEY}\"" >> /config/docker_config.py
 echo "STATIC_ROOT = \"/static/\"" >> /config/docker_config.py
 echo "DEBUG = True" >> /config/docker_config.py
 echo "ALLOWED_HOSTS.append('0.0.0.0')" >> /config/docker_config.py
+echo "EMAIL_HOST = 'smtp'" >> /config/docker_config.py
+echo "EMAIL_PORT = 2525" >> /config/docker_config.py
