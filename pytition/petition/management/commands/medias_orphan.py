9e5d7c2796653709a05572d04735198322d29946
bbmt <bbmt@hotmail.fr>
2020-09-03 13:55:13 +0200

    refactor: convert queryset to list

--- pytition/petition/management/commands/medias_orphan.py
+++ pytition/petition/management/commands/medias_orphan.py
@@ -1,9 +1,7 @@
-from pathlib import Path
+from petition.models import Petition
 from django.core.management.base import BaseCommand
 from django.conf import settings
-
-from petition.models import Petition
-
+from pathlib import Path
 
 
 class Command(BaseCommand):
@@ -11,12 +9,18 @@ class Command(BaseCommand):
     """
     def handle(self, *args, **options):
         mediaroot = Path(settings.MEDIA_ROOT)
-        all_petition = Petition.objects.all()
-        last_petition = all_petition[len(all_petition)-1]
+        all_petition = list(Petition.objects.all())
+        if all_petition:
+            last_petition = all_petition[-1]
+
         for media in mediaroot.glob('**/*.*'):
             media = str(media)
-            for petition in all_petition:
-                if media in petition.text or media in petition.twitter_image:
-                    break
-                if petition == last_petition:
-                    print(media)
+            if all_petition:
+                for petition in all_petition:
+                    if media in petition.text or media in petition.twitter_image:
+                        break
+                    if petition == last_petition:
+                        print(media)
+            else:
+                print(media)
+            
