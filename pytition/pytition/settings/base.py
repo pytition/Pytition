888668a1f5dea72ba21c0a16d700cddd2c745560
Benbb96 <Benbb96@users.noreply.github.com>
2022-08-22 17:57:05 +0200

    Reword comment

--- pytition/pytition/settings/base.py
+++ pytition/pytition/settings/base.py
@@ -277,5 +277,7 @@ MAINTENANCE_MODE_IGNORE_ADMIN_SITE = True
 # Following setting prevents error 500, due to issue #76 of django_maintenance_mode
 MAINTENANCE_MODE_STATE_FILE_PATH = os.path.join(BASE_DIR, 'maintenance_mode_state.txt')
 
-# Set default region to France in order to accept phone numbers without specifying prefix (+33)
+# Set default region to France with regard to accepting phone numbers without international prefix.
+# The +33 prefix for France is therefore optional, but it might be mandatory to add a prefix for 
+# other countries. This does not change anything for translations.
 PHONENUMBER_DEFAULT_REGION = "FR"
